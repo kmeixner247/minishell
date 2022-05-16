0b14e94f9261ea3163f9ccea04d9971f223a6eec
Jonas Subel <jsubel@students.42wolfsburg.de>
2022-05-16 12:06:29 +0200

    exec test

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: jsubel <jsubel@student.42wolfsburg.de >    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 09:35:21 by jsubel            #+#    #+#             */
-/*   Updated: 2022/05/16 09:49:25 by jsubel           ###   ########.fr       */
+/*   Updated: 2022/05/16 12:06:18 by jsubel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,9 @@ int	ft_exec_builtins(char **args)
 	int	result;
 
 	result = 0;
-	if (ft_stcmp(args[0], "echo") == 0)
+	if (ft_strcmp(args[0], "echo") == 0)
 		result = ft_echo(args);
+	if (ft_strcmp(args[0], "pwd") == 0)
+		result = ft_pwd(args);
 	return (result);
-}
-
-int	ft_strcmp(const char *s1, const char *s2)
-{
-	size_t	i;
-
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
-		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
