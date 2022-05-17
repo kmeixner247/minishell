3c5964c0bde69f7670adf80fce576f299cefcd82
Konstantin Meixner <kmeixner@student.42wolfsburg.de>
2022-05-17 16:19:10 +0200

    removed redundant functions

--- src/parsing/utils.c
+++ src/parsing/utils.c
@@ -6,7 +6,7 @@
 /*   By: kmeixner <konstantin.meixner@freenet.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 13:58:39 by kmeixner          #+#    #+#             */
-/*   Updated: 2022/05/16 14:41:03 by kmeixner         ###   ########.fr       */
+/*   Updated: 2022/05/17 16:14:25 by kmeixner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,87 +25,4 @@ int	check_char(unsigned char c, const char *str)
 		str++;
 	}
 	return (0);
-}
-
-t_token	*new_token(void)
-{
-	t_token	*newtoken;
-
-	newtoken = ft_calloc(sizeof(t_token), 1);
-	if (!newtoken)
-		return (NULL);
-	newtoken->args = NULL;
-	newtoken->redir = NULL;
-	newtoken->next = NULL;
-	return (newtoken);
-}
-
-void	token_addback(t_token **start, t_token *new)
-{
-	t_token	*temp;
-
-	temp = *start;
-	if (!temp)
-	{
-		*start = new;
-		return ;
-	}
-	while (temp->next)
-		temp = temp->next;
-	temp->next = new;
-}
-
-t_redir	*new_redir(void)
-{
-	t_redir	*newredir;
-
-	newredir = ft_calloc(sizeof(t_redir), 1);
-	if (!newredir)
-		return (NULL);
-	newredir->id = 0;
-	newredir->filename = NULL;
-	newredir->next = (NULL);
-	return (newredir);
-}
-
-void	redir_addback(t_redir **start, t_redir *new)
-{
-	t_redir	*temp;
-
-	temp = *start;
-	if (!temp)
-	{
-		*start = new;
-		return ;
-	}
-	while (temp->next)
-		temp = temp->next;
-	temp->next = new;
-}
-
-t_args	*new_args(void)
-{
-	t_args	*newargs;
-
-	newargs = ft_calloc(sizeof(t_args), 1);
-	if (!newargs)
-		return (NULL);
-	newargs->arg = NULL;
-	newargs->next = NULL;
-	return (newargs);
-}
-
-void	args_addback(t_args **start, t_args *new)
-{
-	t_args	*temp;
-
-	temp = *start;
-	if (!temp)
-	{
-		*start = new;
-		return ;
-	}
-	while (temp->next)
-		temp = temp->next;
-	temp->next = new;
 }
