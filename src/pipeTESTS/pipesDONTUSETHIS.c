49ed5c9b0dc7d423ec99f497389f17cc4cf1d468
Konstantin Meixner <kmeixner@student.42wolfsburg.de>
2022-05-18 18:27:39 +0200

    here doc is a pain. redirs before forking?

--- src/pipeTESTS/pipesDONTUSETHIS.c
+++ src/pipeTESTS/pipesDONTUSETHIS.c
@@ -6,13 +6,13 @@
 /*   By: kmeixner <konstantin.meixner@freenet.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 16:26:36 by kmeixner          #+#    #+#             */
-/*   Updated: 2022/05/18 14:05:34 by kmeixner         ###   ########.fr       */
+/*   Updated: 2022/05/18 18:07:50 by kmeixner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/minishell.h"
 
-void	trypaths(char **args, char **envp)
+void	try_paths(char **args, char **envp)
 {
 	int		i;
 	char	**paths;
@@ -33,44 +33,87 @@ void	trypaths(char **args, char **envp)
 	free(paths);
 }
 
-void	children(t_token *token, char **envp)
+int	here_doc(char *delimiter)
+{
+	int		writefd;
+	int		readfd;
+	char	*line;
+	char	*tempfilepath;
+	int		openflags;
+	
+	openflags = O_WRONLY | O_CREAT | O_EXCL | O_TRUNC;
+	tempfilepath = ft_strjoin3("/tmp/minishell-thd", ft_itoa((int)&writefd));
+	writefd = open(tempfilepath, openflags, 0600);
+	line = readline("> ");
+	while (42)
+	{
+		if (ft_strlen(delimiter) == ft_strlen(line) && !ft_strncmp(delimiter, line, ft_strlen(line)) || !line)
+			break ;
+		write(writefd, line, ft_strlen(line));
+		write(writefd, "\n", 1);
+		free(line);
+		line = readline("> ");
+	}
+	free(line);
+	readfd = open(tempfilepath, O_RDONLY);
+	close(writefd);
+	return(readfd);
+}
+
+int	redir_input(t_redir *redir, int tempinfd)
+{
+	if (tempinfd > 0)
+		close(tempinfd);
+	if (redir->id == 1)
+		tempinfd = open(redir->filename, O_RDONLY);
+	else if (redir->id == 2)
+		tempinfd = here_doc(redir->filename);
+	return (tempinfd);
+}
+
+int	redir_output(t_redir *redir, int tempoutfd)
+{
+	int		openflags;
+	mode_t	openmodes;
+
+	openflags = 0;
+	openmodes = S_IRWXU | S_IRWXG | S_IRWXO;
+	if (tempoutfd > 0)
+		close(tempoutfd);
+	if (redir->id == 3)
+		openflags = O_WRONLY | O_CREAT | O_TRUNC;
+	else if (redir->id == 4)
+		openflags = O_WRONLY | O_CREAT | O_APPEND;
+	tempoutfd = open(redir->filename, openflags, openmodes);
+	return (tempoutfd);
+}
+
+void	handle_redirs(t_token *token, char **envp)
 {
 	int		tempinfd;
 	int		tempoutfd;
-	char	**args;
-	mode_t	openmodes;
 
-	openmodes = S_IRWXU | S_IRWXG | S_IRWXO;
 	tempinfd = -1;
 	tempoutfd = -1;
 	while (token->redir)
 	{
-		if (token->redir->id == 1)
-		{
-			if (tempinfd > 0)
-				close(tempinfd);
-			tempinfd = open(token->redir->filename, O_RDONLY);
-		}
-		else if (token->redir->id == 2)
-			fprintf(stderr, "This is where here_doc will happen\n");
-		else if (token->redir->id == 3)
-		{
-			if (tempoutfd > 0)
-				close(tempoutfd);
-			tempoutfd = open(token->redir->filename, O_WRONLY | O_CREAT | O_TRUNC, openmodes);
-		}
-		else if (token->redir->id == 4)
-		{
-			if (tempoutfd > 0)
-				close(tempoutfd);
-			tempoutfd = open(token->redir->filename, O_WRONLY | O_CREAT | O_APPEND, openmodes);
-		}
+		if (token->redir->id == 1 || token->redir->id == 2)
+			tempinfd = redir_input(token->redir, tempinfd);
+		else if (token->redir->id == 3 || token->redir->id == 4)
+			tempoutfd = redir_output(token->redir, tempoutfd);
 		token->redir = token->redir->next;
 	}
 	if (tempinfd > 0)
 		token->infd = tempinfd;
 	if (tempoutfd > 0)
 		token->outfd = tempoutfd;
+}
+
+void	children(t_token *token, char **envp)
+{
+	char	**args;
+
+	handle_redirs(token, envp);
 	dup2(token->infd, 0);
 	dup2(token->outfd, 1);
 	if (token->infd != 0)
@@ -78,9 +121,43 @@ void	children(t_token *token, char **envp)
 	if (token->outfd != 1)
 		close(token->outfd);
 	args = get_args(token);
-	trypaths(args, envp);
-	fprintf(stderr, "Hey! I am a child and my first argument is %s\n", token->args->arg);
+	try_paths(args, envp);
 	exit(0);
+}
+
+void	fork_and_execute(t_token *token, char **envp)
+{
+	int pid;
+	int pipefds[2];
+	int wpid;
+
+	pid = 1;
+	while(pid && token->next)
+	{
+		pipe(pipefds);
+		token->outfd = pipefds[1];
+		token->next->infd = pipefds[0];
+		pid = fork();
+		if (!pid)
+			close(pipefds[0]);
+		else
+		{
+			if (token->infd > 0)
+				close(token->infd);
+			close(pipefds[1]);
+			token = token->next;
+		}
+	}
+	if (pid)
+		pid = fork();
+	if (!pid)
+		children(token, envp);
+	else
+	{
+		close(pipefds[0]);
+		while (wpid > 0)
+			wpid = wait(NULL);
+	}
 }
 
 void	exectests(t_token *token, char **envp) 
@@ -95,37 +172,7 @@ void	exectests(t_token *token, char **envp)
 		fprintf(stderr, "no pipes here\n");
 		return ;
 	}
-	while(pid && token->next)
-	{
-		pipe(pipefds);
-		token->outfd = pipefds[1];
-		token->next->infd = pipefds[0];
-		pid = fork();
-		if (!pid)
-		{
-			close(pipefds[0]);
-		}
-		else
-		{
-			if (token->infd > 0)
-				close(token->infd);
-			close(pipefds[1]);
-			token = token->next;
-		}
-	}
-	if (pid)
-		pid = fork();
-	//basically here I can start executing shit
-	if (!pid)
-	{
-		children(token, envp);
-	}
 	else
-	{
-		fprintf(stderr, "I am the almighty parent and my pid is \"%d\"\n", pid);
-		close(pipefds[0]);
-		while (wpid > 0)
-			wpid = wait(NULL);
-	}
+		fork_and_execute(token, envp);
 	return ;
 }
