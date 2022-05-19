721f8fc125a99c28ed36876506282c735188717f
Jonas Subel <jsubel@students.42wolfsburg.de>
2022-05-19 11:49:01 +0200

    some changes

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: jsubel <jsubel@student.42wolfsburg.de >    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 08:58:58 by jsubel            #+#    #+#             */
-/*   Updated: 2022/05/19 09:55:01 by jsubel           ###   ########.fr       */
+/*   Updated: 2022/05/19 10:05:10 by jsubel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,23 @@ int	ft_cd(char **args, t_env **env)
 
 void	ft_change_pwd(char *pwd_old, char *pwd_new, t_env **env)
 {
-	t_env *old;
-	t_env *new;
-
+	t_env	*old;
+	t_env	*new;
+	char	*tmp;
+	
 	old = ft_find_element(*env, "OLDPWD");
 	new = ft_find_element(*env, "PWD");
 	if (old)
-		free(old);
-	ft_lstadd_back(env, old);
+	{
+		free(old->var);
+		tmp = ft_strjoin("OLDPWD=", pwd_old);
+	}
+	else
+	{
+		tmp = ft_strjoin("OLDPWD=", pwd_old);
+		env_addback(env, env_new(tmp)); //exchange env_new for actual function
+		free(tmp);
+	}
 }
 
 t_env	*ft_find_element(t_env *env, char *str)
