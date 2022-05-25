deb254523dfff4fc492972664f6ca8e43777ab3e
Jonas Subel <joooni.su@gmail.com>
2022-05-25 10:50:26 +0200

    added nbr_args back

--- src/parsing/utils_args.c
+++ src/parsing/utils_args.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils_args.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kmeixner <konstantin.meixner@freenet.de    +#+  +:+       +#+        */
+/*   By: jsubel <jsubel@student.42wolfsburg.de >    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 14:07:48 by kmeixner          #+#    #+#             */
-/*   Updated: 2022/05/23 12:07:30 by kmeixner         ###   ########.fr       */
+/*   Updated: 2022/05/23 17:05:20 by jsubel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,4 +63,17 @@ char	**get_args(t_args *args)
 	}
 	retarr[i] = NULL;
 	return (retarr);
+}
+
+int	ft_nbr_args(t_args *args)
+{
+	int	nbr;
+
+	nbr = 0;
+	while (args)
+	{
+		args = args->next;
+		nbr++;
+	}
+	return (nbr);
 }
