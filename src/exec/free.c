97d8aec4e6ca1ce5280218fb86e47e73db68f9f4
Jonas Subel <joooni.su@gmail.com>
2022-05-25 10:04:02 +0200

    added free.c

---
+++ src/exec/free.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsubel <jsubel@student.42wolfsburg.de >    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/23 17:50:32 by jsubel            #+#    #+#             */
+/*   Updated: 2022/05/25 08:57:53 by jsubel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../incl/minishell.h"
+
+void	ft_free_everything(t_shell *shell)
+{
+	if (shell->env)
+		free_env(shell->env);
+	if (shell->token)
+		parsing_cleanup(shell->token);
+/* 	if (shell->raw_input)
+		free(shell->raw_input); */
+}
